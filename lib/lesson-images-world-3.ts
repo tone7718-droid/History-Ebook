b157ca0c7d515374cf1fbee76cd37df65a3aa07e
tone7718-droid <tone7718@gmail.com>
2026-09-15 10:23:23 +0900

    feat: World lesson figures part 3 — early modern, imperialism, world wars, cold war

---
+++ lib/lesson-images-world-3.ts
@@ -0,0 +1,108 @@
+import { u, type LessonImage } from "./lesson-image-helpers";
+
+export const WORLD_LESSON_IMAGES_3: Record<string, LessonImage[]> = {
+  "world/early-modern-europe/exploration-absolutism/age-of-exploration": [
+    u("https://upload.wikimedia.org/wikipedia/commons/c/c2/Portrait_of_a_Man%2C_Said_to_be_Christopher_Columbus.jpg", "콜럼버스", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/2/2e/Ignoto_portoghese%2C_ritratto_di_un_cavaliere_dell%27ordine_di_cristo%2C_1525-50_ca._02.jpg", "바스쿠 다 가마로 대표되는 항해자", "배경"),
+  ],
+  "world/early-modern-europe/exploration-absolutism/americas-conquest": [
+    u("https://upload.wikimedia.org/wikipedia/commons/b/bb/Machu_Picchu%2C_2023_%28012%29.jpg", "마추픽추 — 잉카 문명", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/c/ca/Moctezuma_Mendoza.jpg", "모테쿠소마 2세", "인물"),
+  ],
+  "world/early-modern-europe/exploration-absolutism/western-absolutism": [
+    u("https://upload.wikimedia.org/wikipedia/commons/5/5f/Louis_XIV_of_France.jpg", "루이 14세", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/9/95/Vue_a%C3%A9rienne_du_domaine_de_Versailles_par_ToucanWings_-_Creative_Commons_By_Sa_3.0_-_081_%28cropped%29.jpg", "베르사유 궁전", "배경"),
+  ],
+  "world/early-modern-europe/exploration-absolutism/eastern-absolutism": [
+    u("https://upload.wikimedia.org/wikipedia/commons/d/d6/Inconnu_d%27apr%C3%A8s_J.-M._Nattier%2C_Portrait_de_Pierre_Ier_%28mus%C3%A9e_de_l%E2%80%99Ermitage%29.jpg", "표트르 대제", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/9/95/Vue_a%C3%A9rienne_du_domaine_de_Versailles_par_ToucanWings_-_Creative_Commons_By_Sa_3.0_-_081_%28cropped%29.jpg", "서유럽 절대왕정을 모델로 삼은 궁정 문화", "배경"),
+  ],
+  "world/early-modern-europe/revolution/science-enlightenment": [
+    u("https://upload.wikimedia.org/wikipedia/commons/f/f7/Portrait_of_Sir_Isaac_Newton%2C_1689_%28brightened%29.jpg", "아이작 뉴턴", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/5b/Mainz_Gutenbergdenkmal_2016_%28cropped%29.jpg", "구텐베르크 — 인쇄와 지식 혁명", "배경"),
+  ],
+  "world/early-modern-europe/revolution/english-revolution": [
+    u("https://upload.wikimedia.org/wikipedia/commons/2/24/Oliver_Cromwell_by_Samuel_Cooper.jpg", "올리버 크롬웰", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/8/8f/United_States_Declaration_of_Independence.jpg", "대서양 양안의 권리 선언 전통", "핵심사건(연표)"),
+  ],
+  "world/early-modern-europe/revolution/american-revolution": [
+    u("https://upload.wikimedia.org/wikipedia/commons/8/8f/United_States_Declaration_of_Independence.jpg", "미국 독립선언", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/57/Anonymous_-_Prise_de_la_Bastille.jpg", "대서양 혁명의 다른 축, 바스티유", "핵심사건(연표)"),
+  ],
+  "world/early-modern-europe/revolution/french-revolution": [
+    u("https://upload.wikimedia.org/wikipedia/commons/5/57/Anonymous_-_Prise_de_la_Bastille.jpg", "바스티유 습격", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/50/Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project.jpg", "나폴레옹", "인물"),
+  ],
+  "world/early-modern-europe/revolution/vienna-liberalism-nationalism": [
+    u("https://upload.wikimedia.org/wikipedia/commons/a/ac/Europe_1815_map_en.png", "1815년 빈 체제 이후 유럽", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/50/Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project.jpg", "나폴레옹 전쟁 이후의 재편", "인물"),
+  ],
+  "world/early-modern-europe/revolution/industrial-revolution": [
+    u("https://upload.wikimedia.org/wikipedia/commons/d/dc/Powerloom_weaving_in_1835.jpg", "1835년 동력 직기", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/15/Watt_James_von_Breda.jpg", "제임스 와트", "인물"),
+  ],
+  "world/imperialism-world-wars/imperialism-nationalism/imperialism-partition": [
+    u("https://upload.wikimedia.org/wikipedia/commons/e/e7/African-civilizations-map-pre-colonial.svg", "식민 이전 아프리카 문명권", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/d/d9/%E5%AD%99%E4%B8%AD%E5%B1%B1%E8%82%96%E5%83%8F.jpg", "제국주의에 맞선 아시아 민족 운동", "인물"),
+  ],
+  "world/imperialism-world-wars/imperialism-nationalism/china-national-movement": [
+    u("https://upload.wikimedia.org/wikipedia/commons/d/d9/%E5%AD%99%E4%B8%AD%E5%B1%B1%E8%82%96%E5%83%8F.jpg", "쌓원", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/b/b6/Flag_of_China_%281889%E2%80%931912%29.svg", "신해혁명으로 무너진 청", "배경"),
+  ],
+  "world/imperialism-world-wars/imperialism-nationalism/meiji-japan": [
+    u("https://upload.wikimedia.org/wikipedia/commons/6/65/Mutsuhito-Emperor-Meiji-1873.png", "메이지 천황", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/d/de/Emperor_Meiji_by_Takahashi_Yuichi.jpg", "메이지 유신의 상징", "배경"),
+  ],
+  "world/imperialism-world-wars/imperialism-nationalism/india-national-movement": [
+    u("https://upload.wikimedia.org/wikipedia/commons/7/7a/Mahatma-Gandhi%2C_studio%2C_1931.jpg", "간디와 비폭력 저항", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/1d/Taj_Mahal_%28Edited%29.jpeg", "식민 지배 이전 무굴의 유산", "배경"),
+  ],
+  "world/imperialism-world-wars/imperialism-nationalism/west-asia-africa-se-asia": [
+    u("https://upload.wikimedia.org/wikipedia/commons/a/a8/Ataturk1930s.jpg", "아타튀르크", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/e/e7/African-civilizations-map-pre-colonial.svg", "아프리카·서아시아의 분할 무대", "배경"),
+  ],
+  "world/imperialism-world-wars/world-wars/world-war-i": [
+    u("https://upload.wikimedia.org/wikipedia/commons/e/e6/Bataille_de_Verdun_1916.jpg", "베르뚱 전투 — 제1차 세계 대전", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/e/ea/Gari-Melchers-War-Highsmith.jpeg", "참호전의 이미지", "핵심사건(연표)"),
+  ],
+  "world/imperialism-world-wars/world-wars/russian-revolution": [
+    u("https://upload.wikimedia.org/wikipedia/commons/b/b4/After_the_capture_of_the_Winter_Palace_26_October_1917.jpg", "겨울궁전 점령 이후", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/57/RIAN_archive_850809_General_Secretary_of_the_CPSU_CC_M._Gorbachev_%28crop%29.jpg", "혁명에서 소련 해체로 이어진 체제", "인물"),
+  ],
+  "world/imperialism-world-wars/world-wars/interwar-asia": [
+    u("https://upload.wikimedia.org/wikipedia/commons/2/27/%E8%94%A3%E4%B8%AD%E6%AD%A3%E7%B8%BD%E7%B5%B1%E7%8E%89%E7%85%A7.png", "장제스와 전간기 중국", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/d/d9/%E5%AD%99%E4%B8%AD%E5%B1%B1%E8%82%96%E5%83%8F.jpg", "신해혁명의 유산을 이은 국민당", "배경"),
+  ],
+  "world/imperialism-world-wars/world-wars/great-depression-totalitarianism": [
+    u("https://upload.wikimedia.org/wikipedia/commons/6/6c/Unemployed_men_queued_outside_a_depression_soup_kitchen_opened_in_Chicago_by_Al_Capone%2C_02-1931_-_NARA_-_541927.jpg", "대공황기 배급 행렬", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/10/Bundesarchiv_Bild_101I-646-5188-17%2C_Flugzeuge_Junkers_Ju_87.jpg", "전체주의가 부른 세계 대전", "핵심사건(연표)"),
+  ],
+  "world/imperialism-world-wars/world-wars/world-war-ii": [
+    u("https://upload.wikimedia.org/wikipedia/commons/1/10/Bundesarchiv_Bild_101I-646-5188-17%2C_Flugzeuge_Junkers_Ju_87.jpg", "제2차 세계 대전", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/5d/Berlinermauer.jpg", "전쟁 뒤 갈라진 유럽의 상징", "핵심사건(연표)"),
+  ],
+  "world/contemporary-world/cold-war/cold-war-formation": [
+    u("https://upload.wikimedia.org/wikipedia/commons/5/5d/Berlinermauer.jpg", "베를린 장벽", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/2/2f/Flag_of_the_United_Nations.svg", "국제연합 — 전후 질서", "핵심사건(연표)"),
+  ],
+  "world/contemporary-world/cold-war/cold-war-conflicts": [
+    u("https://upload.wikimedia.org/wikipedia/commons/6/67/U.S._Army_UH-1H_Hueys_insert_ARVN_troops_at_Kh%C3%A2m_%C4%90%E1%BB%A9c%2C_Vietnam%2C_12_July_1970_%2879431435%29.jpg", "베트남 전쟁", "핵심사건(연표)"),
+    u("https://upload.wikimedia.org/wikipedia/commons/9/92/Battle_of_Inchon.png", "한국 전쟁 — 냉전의 열전", "배경"),
+  ],
+  "world/contemporary-world/cold-war/third-world-nonalignment": [
+    u("https://upload.wikimedia.org/wikipedia/commons/9/97/Nehru_in_the_Netherlands%2C_1957.jpg", "네루 — 비동맹 운동", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/7/7a/Mahatma-Gandhi%2C_studio%2C_1931.jpg", "인도 독립 운동의 유산", "배경"),
+  ],
+  "world/contemporary-world/cold-war/detente-end-of-cold-war": [
+    u("https://upload.wikimedia.org/wikipedia/commons/5/57/RIAN_archive_850809_General_Secretary_of_the_CPSU_CC_M._Gorbachev_%28crop%29.jpg", "고르바초프와 냉전 종식", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/5d/Berlinermauer.jpg", "장벽 붕괴로 상징되는 종식", "배경"),
+  ],
+  "world/contemporary-world/twenty-first-century/globalization-multipolarity": [
+    u("https://upload.wikimedia.org/wikipedia/commons/7/70/The_Blue_Marble%2C_AS17-148-22727.jpg", "지구촌으로 연결된 세계", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg", "유럽연합 — 지역 통합의 한 축", "핵심사건(연표)"),
+  ],
+  "world/contemporary-world/twenty-first-century/global-issues": [
+    u("https://upload.wikimedia.org/wikipedia/commons/e/e0/Change_in_Average_Temperature_With_Fahrenheit.svg", "기후 변화", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/2/2f/Flag_of_the_United_Nations.svg", "지구촌 과제를 다루는 국제기구", "인물"),
+  ],
+};
--- lib/lesson-images.ts
+++ lib/lesson-images.ts
@@ -3,6 +3,7 @@ import { KOREAN_LESSON_IMAGES } from "./lesson-images-korean";
 import { KOREAN_LESSON_IMAGES_2 } from "./lesson-images-korean-2";
 import { WORLD_LESSON_IMAGES } from "./lesson-images-world";
 import { WORLD_LESSON_IMAGES_2 } from "./lesson-images-world-2";
+import { WORLD_LESSON_IMAGES_3 } from "./lesson-images-world-3";
 
 export type { LessonImage };
 
@@ -11,6 +12,7 @@ const LESSON_IMAGES: Record<string, LessonImage[]> = {
   ...KOREAN_LESSON_IMAGES_2,
   ...WORLD_LESSON_IMAGES,
   ...WORLD_LESSON_IMAGES_2,
+  ...WORLD_LESSON_IMAGES_3,
 };
 
 export function getLessonImages(lessonKey: string): LessonImage[] {
