a7b43bb01a6a48da2a42391987d0a9d034a03fe8
tone7718-droid <tone7718@gmail.com>
2026-09-15 10:21:49 +0900

    feat: World lesson figures part 2 — East Asia remainder, West Asia, Greece, Rome, medieval

--- lib/lesson-images-world-2.ts
+++ lib/lesson-images-world-2.ts
@@ -1,3 +1,120 @@
 import { u, type LessonImage } from "./lesson-image-helpers";
 
-export const WORLD_LESSON_IMAGES_2: Record<string, LessonImage[]> = {};
+export const WORLD_LESSON_IMAGES_2: Record<string, LessonImage[]> = {
+  "world/east-asia/formation/six-dynasties": [
+    u("https://upload.wikimedia.org/wikipedia/commons/b/b4/27427-Luoyang_%2849067744628%29.jpg", "룡먼 석굴 — 위진남북조·수당 불교", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/2/29/Cheui_Dynasty_581_CE.png", "수 왕조의 통일", "핵심사건(연표)"),
+  ],
+  "world/east-asia/formation/east-asian-cultural-sphere": [
+    u("https://upload.wikimedia.org/wikipedia/commons/1/13/Giant_Wild_Goose_Pagoda.jpg", "장안의 대안탑", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/17/Horyu-ji11s3200.jpg", "호류지 — 동아시아로 퍼진 불교 건축", "핵심사건(연표)"),
+  ],
+  "world/east-asia/development/northern-peoples": [
+    u("https://upload.wikimedia.org/wikipedia/commons/2/25/Liao_circuits.png", "요의 통치 구획", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/6/62/Expansion_of_the_Mongol_Empire_1206%E2%80%931294.gif", "북방 유목 세력의 확대", "핵심사건(연표)"),
+  ],
+  "world/east-asia/transformation/ming": [
+    u("https://upload.wikimedia.org/wikipedia/commons/e/ef/The_Forbidden_City_-_View_from_Coal_Hill.jpg", "자금성", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/b/bc/Map_of_Ming_Chinese_empire_1415_%28cropped_2%29.jpg", "명의 강역", "핵심사건(연표)"),
+  ],
+  "world/east-asia/transformation/qing": [
+    u("https://upload.wikimedia.org/wikipedia/commons/b/b6/Flag_of_China_%281889%E2%80%931912%29.svg", "청의 국기", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/e/ef/The_Forbidden_City_-_View_from_Coal_Hill.jpg", "청이 이은 자금성", "인물"),
+  ],
+  "world/east-asia/transformation/edo-japan": [
+    u("https://upload.wikimedia.org/wikipedia/commons/1/11/Tokugawa_Ieyasu2.JPG", "도쿠가와 이에야스", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/17/Horyu-ji11s3200.jpg", "에도 이전부터 이어진 일본 불교 건축", "배경"),
+  ],
+  "world/east-asia/transformation/east-asia-trade": [
+    u("https://upload.wikimedia.org/wikipedia/commons/d/d2/Bianjing_city_gate.JPG", "동아시아 도시와 상업", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/b/bc/Map_of_Ming_Chinese_empire_1415_%28cropped_2%29.jpg", "명·청기 교역의 무대", "핵심사건(연표)"),
+  ],
+  "world/west-asia/islam/islam-formation": [
+    u("https://upload.wikimedia.org/wikipedia/commons/8/89/The_Ka%27ba%2C_Great_Mosque_of_Mecca%2C_Saudi_Arabia_%284%29.jpg", "카바 — 이슬람의 성지", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/1b/Jerusalem_DomeoftheRock_J65_%28cropped%29.JPG", "바위 돔 — 초기 이슬람 건축", "핵심사건(연표)"),
+  ],
+  "world/west-asia/gunpowder-empires/ottoman-empire": [
+    u("https://upload.wikimedia.org/wikipedia/commons/0/03/Istanbul_%2834223582516%29_%28cropped%29.jpg", "이스탄불의 술탄 아흐메트 모스크", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/4/4a/Hagia_Sophia_%28228968325%29.jpeg", "하기아 소피아 — 비잔티움에서 오스만으로", "인물"),
+  ],
+  "world/west-asia/gunpowder-empires/timurid-safavid": [
+    u("https://upload.wikimedia.org/wikipedia/commons/8/89/The_Ka%27ba%2C_Great_Mosque_of_Mecca%2C_Saudi_Arabia_%284%29.jpg", "이슬람 세계가 공유한 성지", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/c/cf/The_Sasanian_Empire_at_its_apex_under_Khosrow_II.svg", "이란 고원의 앞선 제국 전통", "핵심사건(연표)"),
+  ],
+  "world/west-asia/india/maurya-buddhism": [
+    u("https://upload.wikimedia.org/wikipedia/commons/1/12/East_Gateway_-_Stupa_1_-_Sanchi_Hill_2013-02-21_4398.JPG", "산치 대탑", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/2/28/Ashoka%27s_visit_to_the_Ramagrama_stupa_Sanchi_Stupa_1_Southern_gateway.jpg", "아쇼카의 불탑 순례 부조", "인물"),
+  ],
+  "world/west-asia/india/kushan-gupta": [
+    u("https://upload.wikimedia.org/wikipedia/commons/3/34/Map_of_the_Gupta_Empire.png", "급타 제국의 강역", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/12/East_Gateway_-_Stupa_1_-_Sanchi_Hill_2013-02-21_4398.JPG", "불교 예술이 이어진 산치", "핵심사건(연표)"),
+  ],
+  "world/west-asia/india/delhi-mughal": [
+    u("https://upload.wikimedia.org/wikipedia/commons/1/1d/Taj_Mahal_%28Edited%29.jpeg", "타지마할", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/f/f9/Map_of_the_Mamluk_Dynasty.png", "델리 술탄 왕조의 강역", "핵심사건(연표)"),
+  ],
+  "world/west-asia/india/southeast-asia-indianization": [
+    u("https://upload.wikimedia.org/wikipedia/commons/4/41/Angkor_Wat.jpg", "앙코르와트", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/1/12/East_Gateway_-_Stupa_1_-_Sanchi_Hill_2013-02-21_4398.JPG", "인도에서 전해진 불교·힌두 건축의 원류", "핵심사건(연표)"),
+  ],
+  "world/west-asia/pre-islam-empires/achaemenid-persia": [
+    u("https://upload.wikimedia.org/wikipedia/commons/5/5f/Achaemenid_Empire_500_BCE.jpg", "아케메네스 제국", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/57/Cyrus_II_%28The_Great%29_%28cropped%29.jpg", "키루스 2세", "인물"),
+  ],
+  "world/west-asia/pre-islam-empires/assyria": [
+    u("https://upload.wikimedia.org/wikipedia/commons/a/ab/Ashur_symbol_Nimrud.png", "니묄루드의 아슈르 상징", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/5f/Achaemenid_Empire_500_BCE.jpg", "아시리아 이후 서아시아 제국의 확대", "핵심사건(연표)"),
+  ],
+  "world/west-asia/pre-islam-empires/parthia-sasanian": [
+    u("https://upload.wikimedia.org/wikipedia/commons/c/cf/The_Sasanian_Empire_at_its_apex_under_Khosrow_II.svg", "사산 왕조의 최대 강역", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/5/57/Cyrus_II_%28The_Great%29_%28cropped%29.jpg", "이란 고원 제국의 앞선 전통", "인물"),
+  ],
+  "world/ancient-greece/polis/athenian-democracy": [
+    u("https://upload.wikimedia.org/wikipedia/commons/d/da/The_Parthenon_in_Athens.jpg", "파르테논 — 아테네 폴리스", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/4/4b/Pericles_Pio-Clementino_Inv269_n2.jpg", "페리클레스", "인물"),
+  ],
+  "world/ancient-greece/polis/sparta-persian-wars": [
+    u("https://upload.wikimedia.org/wikipedia/commons/d/db/Spartan_Territory_Before_371_BC.png", "페르시아 전쟁 이전 스파르타 영역", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/e/e1/Thermopylae_ancient_coastline_large.jpg", "테르모필레 지형", "핵심사건(연표)"),
+  ],
+  "world/ancient-greece/polis/hellenism": [
+    u("https://upload.wikimedia.org/wikipedia/commons/4/49/Alexander_Mosaic_detail_of_Alexander_the_Great_%283x4_cropped%29.jpg", "알렉산드로스", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/d/da/The_Parthenon_in_Athens.jpg", "헬레니즘이 확장한 그리스 문화의 원류", "배경"),
+  ],
+  "world/ancient-rome/rome/roman-republic": [
+    u("https://upload.wikimedia.org/wikipedia/commons/6/6a/Foro_Romano_Musei_Capitolini_Roma.jpg", "로마 포럼", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/6/62/Retrato_de_Julio_C%C3%A9sar_%2826724093101%29_%28cropped%29.jpg", "율리우스 카이사르", "인물"),
+  ],
+  "world/ancient-rome/rome/roman-empire": [
+    u("https://upload.wikimedia.org/wikipedia/commons/d/de/Colosseo_2020.jpg", "콜로세움", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/b/b0/Augustus_of_Prima_Porta.jpg", "아우구스투스", "인물"),
+  ],
+  "world/ancient-rome/rome/christianity-rome": [
+    u("https://upload.wikimedia.org/wikipedia/commons/2/28/Constantine_I_The_Great%2C_Colossal_bronze_statue_fragments%2C_Capitoline_Museums.jpg", "콘스탄티누스 대제", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/7/7b/Pantheon_%28Rome%29_-_Right_side_and_front.jpg", "판테온 — 제국의 종교 공간", "배경"),
+  ],
+  "world/medieval-europe/formation-and-church/germanic-franks": [
+    u("https://upload.wikimedia.org/wikipedia/commons/f/fb/Charlemagne_denier_Mayence_812_814.jpg", "샤를마뉴 데나리우스", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/9/9c/Odo_bayeux_tapestry.png", "바이외 태피스트리 — 중세 전사 문화", "배경"),
+  ],
+  "world/medieval-europe/formation-and-church/feudalism": [
+    u("https://upload.wikimedia.org/wikipedia/commons/9/9c/Odo_bayeux_tapestry.png", "봉건 사회의 기사와 주군", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/f/fb/Charlemagne_denier_Mayence_812_814.jpg", "카롤링거 왕권의 유산", "인물"),
+  ],
+  "world/medieval-europe/formation-and-church/papacy-byzantium": [
+    u("https://upload.wikimedia.org/wikipedia/commons/4/4a/Hagia_Sophia_%28228968325%29.jpeg", "하기아 소피아 — 비잔티움", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/3/3d/Mosaic_of_Justinianus_I_-_Basilica_San_Vitale_%28Ravenna%29.jpg", "유스티니아누스", "인물"),
+  ],
+  "world/medieval-europe/formation-and-church/crusades": [
+    u("https://upload.wikimedia.org/wikipedia/commons/f/f5/Combat_deuxi%C3%A8me_croisade.jpg", "십자군 전투 장면", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/0/0e/Peter_the_Hermit.jpg", "은수자 피에르 — 1차 십자군", "인물"),
+  ],
+  "world/medieval-europe/late-medieval-reform/late-medieval-crisis": [
+    u("https://upload.wikimedia.org/wikipedia/commons/4/4e/1346-1353_spread_of_the_Black_Death_in_Europe_map.svg", "흑사병의 확산", "배경"),
+    u("https://upload.wikimedia.org/wikipedia/commons/b/be/Schlacht_von_Azincourt.jpg", "아쟁쿠르 — 백년 전쟁", "핵심사건(연표)"),
+  ],
+  "world/medieval-europe/late-medieval-reform/renaissance-reformation": [
+    u("https://upload.wikimedia.org/wikipedia/commons/9/90/Lucas_Cranach_d.%C3%84._-_Martin_Luther%2C_1528_%28Veste_Coburg%29.jpg", "마르틴 루터", "인물"),
+    u("https://upload.wikimedia.org/wikipedia/commons/c/cb/Francesco_Melzi_-_Portrait_of_Leonardo.png", "레오나르도 다빈치 — 르네상스", "배경"),
+  ],
+};
